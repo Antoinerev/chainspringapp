69ba8a05cb2b75501f843516da7c8c4435a40b92
Antoine Reveau <null>
2018-11-30 18:19:55 +0100

    tentatives to modify references with link appearance

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,8 +1,9 @@
 class Node
   include ActiveModel::Model
   include ActiveModel::Serializers::JSON
+  # include Rails.application.routes.url_helpers
 
-  attr_accessor :name, :_color, :object_id, :object_type, :link
+  attr_accessor :name, :_color, :object_id, :object_type, :link, :_cssClass #, :svgSym
   attr_reader :id
 
   validates_presence_of :name
@@ -16,16 +17,23 @@ class Node
       _color: nil,
       object_id: nil,
       object_type: nil,
-      link: nil
+      link: nil,
+      # svgSym: nil
+      _cssClass: nil
     }
   end
 
   def initialize(params)
-    @name = params[:name]
+    if params[:link].present?
+      @name = params[:name] + " - click on node for external link"
+    else
+      @name = params[:name]
+    end
     @_color = params[:_color] || ""
     @object_id = params[:id]
     @object_type = params[:type]
     @link = params[:link] || ""
+    @_cssClass = params[:link].present? ? 'ref-link' : ''
 
     if self.valid?
       @id = new_id
@@ -36,6 +44,14 @@ class Node
   end
 
   private
+
+  # def svgSym
+  #   if @link.present?
+  #     return ActionController::Base.helpers.image_url("ext_link.png")
+  #   else
+  #     return ""
+  #   end
+  # end
 
   def new_id
     @@id+=1
